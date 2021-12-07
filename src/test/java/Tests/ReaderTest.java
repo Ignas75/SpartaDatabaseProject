087cf030dbe194cbfc05fb58d9a753d3bee505a7
Tanasa Silviu <tanasasilviu@gmail.com>
2021-12-07 12:35:40 +0000

    Updated the unit test for the Reader class

--- src/test/java/Tests/ReaderTest.java
+++ src/test/java/Tests/ReaderTest.java
@@ -4,31 +4,19 @@ import com.database.employee.Employee;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.database.reader.Reader;
-
-import static org.junit.jupiter.api.Assertions.fail;
-
 public class ReaderTest
 {
     @Test
-    public void ReaderTest()
+    public void testReader()
     {
-        File file = new File("asd");
-
-//        try
-//        {
-//            Reader reader = new Reader();
-//            List<Employee> list = new ArrayList<>();
-//            reader.readCSV(file, list);
-//            fail("exception not thrown");
-//        }
-//        catch(IOException e)
-//        {
-//
-//        }
+        File file = new File("testCSV.csv");
+        Reader reader = new Reader();
+        List<Employee> list = new ArrayList<>();
+        reader.readCSV(file, list);
+        assert(list.size() == 19);
     }
 }
