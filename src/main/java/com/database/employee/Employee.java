f5f5398559e1b5586b8501d82633a6bd434fc967
leo-hoang <leohoang92@gmail.com>
2021-12-07 10:04:07 +0000

    Employee started

--- src/main/java/com/database/employee/Employee.java
+++ src/main/java/com/database/employee/Employee.java
@@ -1,9 +1,35 @@
 package com.database.employee;
 
-public class Employee
-{
-    public Employee(String line)
-    {
+public class Employee implements Comparable {
 
+    private int ID;
+    private String title;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String gender;
+    private String email;
+    private String dob;
+    private String joinDate;
+    private int salary;
+
+    public Employee(String entry)
+    {
+        String[] arr = entry.split(",");
+        ID = Integer.parseInt(arr[0]);
+        title = arr[1];
+        firstName = arr[2];
+        middleName = arr[3];
+        lastName = arr[4];
+        gender = arr[5];
+        email = arr[6];
+        dob = arr[7];
+        joinDate = arr[8];
+        salary = Integer.parseInt(arr[9]);
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        return 0;
     }
 }
