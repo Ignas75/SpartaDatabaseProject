882a820418f78f560463a317917d6544b24e06d2
Ignas75 <ignasserepka75@gmail.com>
2021-12-09 14:00:36 +0000

    added latest ReaderTest.java version

--- src/test/java/Tests/ReaderTest.java
+++ src/test/java/Tests/ReaderTest.java
@@ -22,27 +22,8 @@ public class ReaderTest
         Reader reader = new Reader();
         List<Employee> list = new ArrayList<>();
         reader.readCSV(file);
-        assert(reader.getData().size() == 19);
-    }
-
-    @Test
-    @DisplayName("Given")
-    public void FilterTest(){
-        List<Employee> input = new ArrayList<>();
-        String[] employeeTestStrings = {
-                "198429,Mrs.,Serafina,I,Bumgarner,F,serafina.bumgarner@exxonmobil.com,9/21/1982,2/1/2008,69294",
-                "260736,Ms.,Zelda,P,Forest,F,zelda.forest@ibm.com,11/27/1959,1/28/2014,176642",
-                "127957,Ms.,Nelia,T,Lopez,F,nelia.lopez@gmail.com,1/9/1989,7/9/2011,88806",
-                "198429,Mrs.,Serafina,I,Bumgarner,F,serafina.bumgarner@exxonmobil.com,9/21/1982,2/1/2008,69294",
-                "127957,Ms.,Nelia,T,Lopez,F,nelia.lopez@gmail.com,1/9/1989,7/9/2011,88806",
-                "127957,Ms.,Nelia,T,Lopez,F,nelia.lopez@gmail.com,1/9/1989,7/9/2011,88806"
-        };
-        int numDistinct = 3; // should be equal to the number of distinct strings in the array above
-        for(String str: employeeTestStrings){
-            input.add(new Employee(str));
-        }
-        Reader reader = new Reader();
-        List<Employee> filteredList = reader.filterDuplicates(input);
-        assertEquals(numDistinct, filteredList.size());
+        assert(reader.getFilteredData().size() == 19);
+        assert(reader.getCorruptedDataCounter() == 3);
+        assert(reader.getDuplicateDataCounter() == 4);
     }
 }
