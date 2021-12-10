1aba0b353b4d775a291b702778e9034533cdce45
Tanasa Silviu <tanasasilviu@gmail.com>
2021-12-10 14:57:06 +0000

    Added method in controller for batch insertion

--- src/main/java/com/database/controller/DatabaseController.java
+++ src/main/java/com/database/controller/DatabaseController.java
@@ -5,22 +5,25 @@ import com.database.reader.Reader;
 import com.database.sqlmanager.SQLObject;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class DatabaseController
 {
     private Reader reader;
     private SQLObject sqlObject;
+
     public void parseCSV(String filePath)
     {
         reader = new Reader();
 
-        if(!isValidFileType(filePath))
+        if (!isValidFileType(filePath))
         {
             System.out.println("Invalid file extension");
             //TODO add logging here
-        }
-        else
+        } else
         {
             File file = new File(filePath);
             reader.readCSV(file);
@@ -52,4 +55,28 @@ public class DatabaseController
     {
         return path.endsWith(".csv") || path.endsWith(".txt");
     }
+
+    public void batchInsert(HashSet<Employee> set, int splits)
+    {
+
+        List<HashSet<Employee>> theSets = new ArrayList<HashSet<Employee>>(splits);
+        for (int i = 0; i < splits; i++)
+        {
+            theSets.add(new HashSet<Employee>());
+        }
+
+        int index = 0;
+        for (Employee object : set)
+        {
+            theSets.get(index++ % splits).add(object);
+        }
+
+        for(HashSet<Employee> e :theSets)
+        {
+            SQLObject connection = new SQLObject();
+            connection.establishConnection();
+            connection.batchInsert(e);
+        }
+    }
+
 }
