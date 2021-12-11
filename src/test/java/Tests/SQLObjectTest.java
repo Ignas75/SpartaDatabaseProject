7eca61802b684b70c3a1c317324ad471f0e8e828
leo-hoang <leohoang92@gmail.com>
2021-12-11 15:49:20 +0000

    SQLObject can now create DB, insert and insert batch! Small tests done.

--- src/test/java/Tests/SQLObjectTest.java
+++ src/test/java/Tests/SQLObjectTest.java
@@ -1,25 +1,50 @@
 package Tests;
 
+import com.database.employee.Employee;
 import com.database.sqlmanager.SQLObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SQLObjectTest {
     private SQLObject sqlo;
+    private Employee employee1;
+    private Employee employee2;
+    private Employee employee3;
+    HashSet<Employee> employees;
 
     @BeforeEach
     public void setUp() {
+        // Create database + table
         sqlo = new SQLObject();
         sqlo.establishConnection();
         sqlo.createDatabase();
+
+        // Initialize Objects
+        employee1 = new Employee("198429,Mrs.,Serafina,I,Bumgarner,F,serafina.bumgarner@exxonmobil.com,9/21/1982,2/1/2008,69294");
+        employee2 = new Employee("178566,Mrs.,Juliette,M,Rojo,F,juliette.rojo@yahoo.co.uk,5/8/1967,6/4/2011,193912");
+        employee3 = new Employee("647173,Mr.,Milan,F,Krawczyk,M,milan.krawczyk@hotmail.com,4/4/1980,1/19/2012,123681");
+
+        employees = new HashSet<>(Arrays.asList(employee1, employee2, employee3));
     }
 
     @Test
-    @DisplayName("createDatabase")
-    public void createDatabase()
+    @DisplayName("insertEmployee")
+    public void insertEmployee()
     {
+        sqlo.InsertStatement(employee1);
+    }
 
+    @Test
+    @DisplayName("insertBatchEmployees")
+    public void insertBatchEmployees()
+    {
+        sqlo.batchInsert(employees);
     }
 }
